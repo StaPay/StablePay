1153c1589c361236727a8631dc6cdc291dd4fb28
danyr <danyr@DESKTOP-5DKEN75>
2023-05-08 20:36:03 -0500

    carpeta front

--- backend/contracts/SwapToStable.sol
+++ backend/contracts/SwapToStable.sol
@@ -16,25 +16,27 @@ contract SwapToStable {
     iWMATIC public cWMATIC;
     
     //Address Contracts
-    address public constant WDAI=0x001B3B4d0F3714Ca98ba10F6042DaEbF0B1B7b6F;// Polygon 0xaFfc274413Ec9b972BE1E63c3aA2d8189E90eeCB; //0x19D66Abd20Fb2a0Fc046C139d5af1e97F09A695e; USDC Mumbai // Mainnet 0x6B175474E89094C44Da98b954EedeAC495271d0F; 
-    address public constant WMATIC= 0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889;// Polygon 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270; //0x2a655231e814e71015ff991d90c5790B5dE82B94; WMATIC Mumbai // Mainnet 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; // 
+    address public constant WDAI=0x001B3B4d0F3714Ca98ba10F6042DaEbF0B1B7b6F; // Polygon // Mainnet WDAI 0x6B175474E89094C44Da98b954EedeAC495271d0F; 
+    address public constant WMATIC= 0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889; // Polygon // Mainnet WETH 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public constant WETH=0xFD2AB41e083c75085807c4A65C0A14FDD93d55A9; // polygon
     address public constant addressRouter=0xE592427A0AEce92De3Edee1F18E0157C05861564;
 
     //State Variables 
     address payable public owner; //Contract's owner
     uint256 public amountGoal;
     uint256 public amountApprove;
-    uint256 public pool;
+    uint256 public poolMatic;
+    uint256 public poolEth;
     string public title;
     State public state;
     uint24 public constant feeTier= 3000; // 500, 3000, 10000 
-    //uint256 public currentBalancePool;
+    //uint256 public currentBalancePoolMatic;
 
     //mapping
     mapping (address => uint) public client;
 
     enum State {
-             RisingPool,
+             RisingPoolMatic,
              AchieveGoal
     }
 
@@ -47,25 +49,25 @@ contract SwapToStable {
 
         router = ISwapRouter(addressRouter);  
         cWMATIC = iWMATIC(WMATIC);
-        state = State.RisingPool;
+        state = State.RisingPoolMatic;
         approveWMATIC(amountApprove);
 
     }
 
     function pay() public payable {
         require(msg.value>0, "Insufficient tokens");
-        pool = pool + msg.value;
+        poolMatic = poolMatic + msg.value;
         checkIfAchieveGoal();
     }
 
     receive() external payable {
-        pool = pool + msg.value;
+        poolMatic = poolMatic + msg.value;
         checkIfAchieveGoal();
     }
 
 
     function checkIfAchieveGoal() internal {
-        if (pool>amountGoal) {
+        if (poolMatic>amountGoal) {
             state = State.AchieveGoal;
             swapETHtoDAI();
         } 
@@ -80,15 +82,15 @@ contract SwapToStable {
 
     function swapETHtoDAI() public payable returns (uint256) {
         
-        require(pool>=address(this).balance && pool>0, "Insufficient funds prr");
+        require(poolMatic>=address(this).balance && poolMatic>0, "Insufficient funds prr");
         require(state ==State.AchieveGoal,"Not Achieve Goal prr");
         //debe haber una advertencia si la transaccion ha sido revertida por no aprobar la transferencia de WMATIC por el contrato
 
         
-        cWMATIC.deposit{value:pool}();
+        cWMATIC.deposit{value:poolMatic}();
         // cWMATIC.approve(address(this),msg.value);
 
-        // TransferHelper.safeTransferFrom(WMATIC, msg.sender, address(this), pool);
+        // TransferHelper.safeTransferFrom(WMATIC, msg.sender, address(this), poolMatic);
 
         uint256 minOut= 0;
         uint160 priceLimit=0;
@@ -99,13 +101,13 @@ contract SwapToStable {
                                                         fee:feeTier,
                                                         recipient:owner,
                                                         deadline:block.timestamp,
-                                                        amountIn:pool,
+                                                        amountIn:poolMatic,
                                                         amountOutMinimum:minOut,
                                                         sqrtPriceLimitX96:priceLimit
                                                         });
 
         uint256 amountOut = router.exactInputSingle(params);
-        pool=0;
+        poolMatic=0;
         return amountOut;
         
     }
@@ -119,7 +121,7 @@ contract SwapToStable {
                                                 uint256
                                                 ) {
 
-        return(owner, amountGoal, amountApprove, pool, title, state, feeTier); 
+        return(owner, amountGoal, amountApprove, poolMatic, title, state, feeTier); 
    }
 
 

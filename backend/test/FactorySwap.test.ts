27002b7a7441b845423be3cd7ab473f0dc86c6e0
danyr <danyr@DESKTOP-5DKEN75>
2023-06-01 00:18:22 -0500

    pagina crear contrato 2

--- backend/test/FactorySwap.test.ts
+++ backend/test/FactorySwap.test.ts
@@ -2,6 +2,7 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 import { dataContractFactory } from "../dataContractFactory";
 
+
 describe("Test FactorySwaptoStable", ()=> {
 
      it('Probando funsion contructorContract', async function () {
@@ -17,18 +18,26 @@ describe("Test FactorySwaptoStable", ()=> {
           console.log('Address del contrato =>>',factorySwapToStable.address )
 
           //usando la funcion contructorContract
-          const receipt = await factorySwapToStable.contructorContract("Tambos",owner.address, ethers.utils.parseEther("0.0001"))
-          await receipt.wait()    
-          console.log("Hash de la tx- creacion de un nuevo contrato",receipt.hash);
+          const newContractReceipt = await factorySwapToStable.contructorContract("Tambos",owner.address, ethers.utils.parseEther("0.0001"))
+          
+          await newContractReceipt.wait()    
+          console.log("Hash de la tx newContractReceipt ==> ",await newContractReceipt.wait(2));
+          console.log("Hash de la tx- creacion de un nuevo contrato ==> ",newContractReceipt.hash);
 
-          console.log("oner address =>", owner.address);
           
           const contractsUsers1 = await factorySwapToStable.getContractsOfUsers(owner.address)
           console.log(`contrato del user1 ${owner.address} => `, contractsUsers1 );
           
-          const contractsUsers2 = await factorySwapToStable.getContractsOfUsers(user2.address)
+          const contractsUsers2 = await factorySwapToStable.getContractsOfUsers(owner.address)
           console.log(`contrato del user2 ${user2.address} => `, contractsUsers2 );
         
+          
+
+          // Para usar mapping que apunta un array, aparte de la key, se requiere un indice del array como argumento
+          const receipt2 = await factorySwapToStable.users(owner.address, 1)
+          console.log("mapping user => ", receipt2);
+          
+
 
      });
 

8dda1fafc3a94fb539a0d40124194f976134d95b
Rudy Ramirez <ruramirez@nuevatel.com>
2023-06-09 15:55:47 -0400

    Nuevas vistas para la demo

--- frontend/pages/settings.jsx
+++ frontend/pages/settings.jsx
@@ -2,14 +2,90 @@ import React from 'react'
 
 export default function Settings() {
   return (
-    <div className='m-32'>
+    <div>      
+      <form 
+        className="flex flex-col items-stretch md:w-5/12 mx-auto sm:w-full"
+        onSubmit={(e) => {{
+            e.preventDefault();
+                          }}    
+            }>
 
+        <div className="mb-6">
+          <label
+            htmlFor="nombreEmpresa"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Nombre de tu empresa
+          </label>
+          <input
+            type="text"
+            id="nombreEmpresa"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Hooli"
+            required
+          />
+        </div>
 
-    <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight
-     text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center">
-     Comming Soon</h1>
-    
+        <div className="mb-6">
+          <label
+            htmlFor="goalMatic"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            ¿Cuánto Matic como máximo deseas que se almacene en el contrato
+            inteligente?
+          </label>
+          <input
+            type="number"
+            min="0"
+            step="any"
+            id="goalMatic"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="0.001"
+            required
+          />
+        </div>
 
+        <div className="mb-6">
+          <label
+            htmlFor="stablecoins"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Seleciona una criptomoneda estable
+          </label>
+
+          <select
+            name="stablecoins"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="DAI"
+          >
+            <option value="manzana">DAI</option>
+          </select>
+        </div>
+
+        <div className="mb-6">
+          <label
+            htmlFor="Apitoken"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Token Api
+          </label>
+          <input
+            type="text"
+            id="Apitoken"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmF"
+            required
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Guardar
+        </button>
+      </form>
     </div>
-  )
+  );
+
 }
